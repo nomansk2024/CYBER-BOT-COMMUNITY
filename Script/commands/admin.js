55af2464f4a3483d26d319f8215d3337ccd51458
nomansk2024 <menomansk@gmail.com>
2025-06-24 20:16:39 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -7,7 +7,7 @@ module.exports.config = {
     name: "admin",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "NOMAN SHEIKH", //don't change my credit 
+    credits: "ULLASH", //don't change my credit 
     description: "Show Owner Info",
     commandCategory: "info",
     usages: "",
@@ -24,14 +24,14 @@ module.exports.run = async function({ api, event }) {
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 👤 𝐍𝐚𝐦𝐞      : NOMAN SHEIKH 
 ┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐈𝐧 𝐂𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝
-┃ 🎂 𝐀𝐠𝐞       : 20
+┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : Single pro max 
+┃ 🎂 𝐀𝐠𝐞       : 21
 ┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : Honours 1st year rupsha college
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Khulna, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : Honours 1st year Rupsha college 
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Khulna , 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : nomansk2025
-┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : দিব না 🤧 
+┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : nai
 ┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/nomansk2025
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
@@ -40,6 +40,7 @@ module.exports.run = async function({ api, event }) {
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    return request(encodeURI('https://www.facebook.com/share/p/1DkXvj7XHr/'))
+    return request(encodeURI(`https://www.facebook.com/share/p/1DkXvj7XHr/`))
+        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
         .on('close', () => callback());
 };
